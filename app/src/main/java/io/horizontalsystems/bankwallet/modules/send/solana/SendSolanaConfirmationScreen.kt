5e50afdeba070dcb186d1854bb8c31a77e9a3950
Daniil Kirillov <danik.mad@gmail.com>
2025-09-11 18:00:58 +0200

    added getting unsigned Solana tx for hardware signing

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/send/solana/SendSolanaConfirmationScreen.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/send/solana/SendSolanaConfirmationScreen.kt
@@ -1,14 +1,27 @@
 package io.horizontalsystems.bankwallet.modules.send.solana
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.compose.LifecycleResumeEffect
 import androidx.navigation.NavController
+import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.modules.amount.AmountInputModeViewModel
+import io.horizontalsystems.bankwallet.modules.hardwarewallet.LoadingScreen
 import io.horizontalsystems.bankwallet.modules.send.SendConfirmationScreen
+import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
+import io.horizontalsystems.bankwallet.ui.compose.components.AppBar
+import io.horizontalsystems.bankwallet.ui.compose.components.HsBackButton
 
 @Composable
 fun SendSolanaConfirmationScreen(
@@ -19,6 +32,46 @@ fun SendSolanaConfirmationScreen(
 ) {
     var confirmationData by remember { mutableStateOf(sendViewModel.getConfirmationData()) }
     var refresh by remember { mutableStateOf(false) }
+
+    val unsignedTxHex by sendViewModel.unsignedTxHex.collectAsState()
+
+    LaunchedEffect(Unit) {
+        if (sendViewModel.isHardwareAccount()) {
+            sendViewModel.getUnsignedTransaction(
+                to = confirmationData.address.toString(),
+                amount = confirmationData.amount
+            )
+        }
+    }
+
+    if (unsignedTxHex == null) {
+        Column(Modifier.background(color = ComposeAppTheme.colors.tyler)) {
+            AppBar(
+                title = stringResource(R.string.Send_Confirmation_Title),
+                navigationIcon = {
+                    HsBackButton(onClick = { navController.popBackStack() })
+                },
+                menuItems = listOf()
+            )
+            Box(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                AppBar(
+                    title = stringResource(R.string.Send_Confirmation_Title),
+                    navigationIcon = {
+                        HsBackButton(onClick = { navController.popBackStack() })
+                    },
+                    menuItems = listOf()
+                )
+                LoadingScreen(
+                    navController = navController,
+                    loadingMessage = "Creating transaction..."
+                )
+            }
+        }
+        return
+    }
+
 
     LifecycleResumeEffect(Unit) {
         if (refresh) {
@@ -40,6 +93,7 @@ fun SendSolanaConfirmationScreen(
         sendResult = sendViewModel.sendResult,
         blockchainType = sendViewModel.blockchainType,
         coin = confirmationData.coin,
+        sendEntryPointDestId = sendEntryPointDestId,
         feeCoin = confirmationData.feeCoin,
         amount = confirmationData.amount,
         address = confirmationData.address,
@@ -49,6 +103,8 @@ fun SendSolanaConfirmationScreen(
         memo = confirmationData.memo,
         rbfEnabled = confirmationData.rbfEnabled,
         onClickSend = sendViewModel::onClickSend,
-        sendEntryPointDestId = sendEntryPointDestId
+        onScannedQR = sendViewModel::setScannedQr,
+        isHardwareSigner = sendViewModel.isHardwareAccount(),
+        unsignedTxHex = unsignedTxHex
     )
 }
