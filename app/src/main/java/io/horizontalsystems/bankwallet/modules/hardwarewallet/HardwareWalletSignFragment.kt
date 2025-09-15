f58d89261962d52e174282f0af0d40bde411ff80
Daniil Kirillov <danik.mad@gmail.com>
2025-09-15 14:51:51 +0200

    changed ui of HardwareWalletSendCautions

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/hardwarewallet/HardwareWalletSignFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/hardwarewallet/HardwareWalletSignFragment.kt
@@ -37,6 +37,7 @@ import io.horizontalsystems.bankwallet.modules.evmfee.eip1559.Eip1559FeeSettings
 import io.horizontalsystems.bankwallet.modules.send.evm.confirmation.SendEvmConfirmationViewModel
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 import io.horizontalsystems.bankwallet.ui.compose.components.ButtonPrimaryYellow
+import io.horizontalsystems.bankwallet.ui.compose.components.cell.SectionUniversalLawrence
 import io.horizontalsystems.ethereumkit.core.hexStringToByteArray
 import io.horizontalsystems.ethereumkit.models.Signature
 import io.horizontalsystems.ethereumkit.spv.core.toBigInteger
@@ -45,7 +46,7 @@ import io.horizontalsystems.ethereumkit.spv.rlp.RLP
 import io.horizontalsystems.ethereumkit.spv.rlp.RLPList
 import io.horizontalsystems.core.helpers.HudHelper
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun Preview_HardwareWalletSignScanFragment() {
     ComposeAppTheme {
@@ -57,34 +58,36 @@ fun Preview_HardwareWalletSignScanFragment() {
 @Composable
 fun HardwareWalletSendCautions(modifier: Modifier) {
     val fontSize = 16.sp
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Please check the transaction data on your device",
-            style = ComposeAppTheme.typography.title1,
-            color = ComposeAppTheme.colors.leah,
-            fontSize = 1.2 * fontSize,
-            textAlign = TextAlign.Center,
-            modifier = modifier
-        )
-//        Spacer(Modifier.height(16.dp))
-        Text(
-            text = "After checking your transaction data:",
-            style = ComposeAppTheme.typography.body,
-            color = ComposeAppTheme.colors.leah,
-            fontSize = fontSize,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.padding(horizontal = 16.dp)
-        )
-        DottedList(
-            modifier.padding(horizontal = 8.dp),
-            listOf(
-                "Press 'Sign' on your hardware device",
-                "Press 'Continue' on your phone to scan the QR code appeared"
+    SectionUniversalLawrence {
+        Column(
+            modifier = modifier,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Please check the transaction data on your device",
+                style = ComposeAppTheme.typography.headline1,
+                color = ComposeAppTheme.colors.leah,
+                fontSize = 1.2 * fontSize,
+                textAlign = TextAlign.Center,
+                modifier = modifier
             )
-        )
+//        Spacer(Modifier.height(16.dp))
+            Text(
+                text = "After checking your transaction data:",
+                style = ComposeAppTheme.typography.body,
+                color = ComposeAppTheme.colors.leah,
+                fontSize = fontSize,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+            DottedList(
+                modifier.padding(horizontal = 8.dp),
+                listOf(
+                    "Press 'Sign' on your hardware device",
+                    "Press 'Continue' on your phone to scan the QR code appeared"
+                )
+            )
+        }
     }
 }
 
@@ -108,136 +111,6 @@ fun HardwareWalletSignScanFragment(
                 onContinueClick = onContinueClick,
                 onTryAgainClick = onTryAgainClick
             )
-        }
-    }
-}
-
-@Composable
-fun HardwareWalletSignFragment(
-    modifier: Modifier,
-    ownAddress: String,
-    sendViewModel: SendEvmConfirmationViewModel? = null,
-    feeModel: Eip1559FeeSettingsViewModel? = null,
-) {
-
-    val logger = AppLogger("sign-evm-hardware")
-    var qrScannerFinished by remember { mutableStateOf(false) }
-
-    val vm: HardwareWalletSignViewModel = androidx.lifecycle.viewmodel.compose.viewModel(
-        factory = HardwareWalletSignViewModel.Factory(sendViewModel?.sendTransactionService)
-    )
-
-    val state by vm.unsignedHex.collectAsState(initial = DataState.Loading)
-
-    var isSending by remember { mutableStateOf(false) }
-    var isTxLoaded by remember { mutableStateOf(false) }
-
-    var scanToTransmit by remember { mutableStateOf(false) }
-    var txData by remember { mutableStateOf<String?>(null) }
-    var messageHex by remember { mutableStateOf<String?>(null) }
-
-//    LaunchedEffect(signMessageViewModel) {
-//        if (signMessageViewModel != null) {
-//            messageHex = signMessageViewModel?.message?.data?.toByteArray().toHexString()
-//        } else {
-//            messageHex = null
-//        }
-//    }
-
-    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == Activity.RESULT_OK) {
-            qrScannerFinished = true
-            val scannedQr = result.data?.getStringExtra(ModuleField.SCAN_ADDRESS)?: ""
-            if (scannedQr.startsWith("https://app.hito.dev/eth/tx/#!")) {
-                val txHex = scannedQr.removePrefix("https://app.hito.dev/eth/tx/#!")
-                val signature = decodeRawTransactionSignature(txHex)
-                val signatureHex = signature?.toHex()
-                isSending = true
-                sendViewModel?.sendAsync(signatureHex)
-            } else if (scannedQr.startsWith("evm.sig:")) {
-                val signatureHex = scannedQr.removePrefix("evm.sig:")
-                isSending = true
-                if (messageHex == null) {
-                    sendViewModel?.sendAsync(signatureHex)
-                } else {
-                    val signature = signatureHex.toSignature()
-                    val shex = signature?.toByteArray().toHexString()
-                    // TODO: handle case when signature is null
-//                    signMessageViewModel?.acceptWithSignature(shex!!)
-                }
-            } else {
-                //TODO todo handle error
-//                sendViewModel?.service?.setFailed(IOException("Signature Scan Error"))
-//                signMessageViewModel?.showSignError = true
-            }
-        }
-    }
-
-    // context
-    val context = LocalContext.current
-    LaunchedEffect(Unit) { vm.loadUnsigned() }
-    when (val s = state) {
-        DataState.Loading -> LoadingScreen(loadingMessage = "Creating Transaction...")
-        is DataState.Success -> {
-            txData = s.data
-            isTxLoaded = true
-            sendViewModel?.sendTransactionService?.pauseSync()
-            feeModel?.pauseSync()
-        }
-        is DataState.Error -> {
-            isTxLoaded = false
-            HudHelper.showErrorMessage(
-                contenView = LocalView.current,
-                resId = R.string.HardwareWallet_TransactionError,
-                icon = R.drawable.icon_24_warning_2,
-                iconTint = R.color.white
-            )
-        }
-    }
-    val view = LocalView.current
-    var nfcWritingStarted by remember { mutableStateOf(false) }
-    val nfcHandler = HardwareWalletNFCHandler(context, onSuccess = {
-        scanToTransmit = true
-        nfcWritingStarted = false
-    }, onError = {        HudHelper.showErrorMessage(
-        contenView = view,
-        resId = R.string.HardwareWalletAuthentication_TagError,
-        icon = R.drawable.icon_24_warning_2,
-        iconTint = R.color.white
-    )})
-    if (isTxLoaded && !isSending) {
-        Column(
-            modifier = modifier,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Spacer(Modifier.height(8.dp))
-
-            if (!scanToTransmit) {
-                if (nfcWritingStarted) {
-                    val messageText = if (messageHex != null) {
-                        "evm.msg:$ownAddress:$messageHex"
-                    } else {
-                        "evm.sign:$ownAddress:$txData"
-                    }
-                    val nfcCallback = NFCCallback(
-                        type = NFCCallbackType.ETH_SEND,
-                        messageText = messageText
-                    )
-                    StartNFCWriting(nfcHandler, nfcCallback)
-
-                    AnimatedNFCBox(
-                        onCancelClick = { nfcWritingStarted = false },
-                        text = "Confirm by tapping Hito Wallet"
-                    )
-                } else {
-                    ButtonPrimaryYellow(
-                        modifier = Modifier.fillMaxWidth(),
-                        title = "Send",
-                        onClick = { nfcWritingStarted = true }
-                    )
-                }
-            } else {
-            }
         }
     }
 }
