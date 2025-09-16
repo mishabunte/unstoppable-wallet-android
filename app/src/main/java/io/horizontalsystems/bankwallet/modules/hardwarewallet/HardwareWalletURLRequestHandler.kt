86d6cd39d77eed723c935a1d79f4ca37e93a6186
Daniil Kirillov <danik.mad@gmail.com>
2025-09-16 13:48:26 +0200

    url handling in withContext(Dispatcher.IO) by default

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/hardwarewallet/HardwareWalletURLRequestHandler.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/hardwarewallet/HardwareWalletURLRequestHandler.kt
@@ -2,10 +2,8 @@ package io.horizontalsystems.bankwallet.modules.hardwarewallet
 
 import android.util.Log
 import com.google.gson.Gson
-import io.horizontalsystems.bitcoincore.crypto.Base58
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.json.JSONObject
 import java.io.IOException
 import java.net.HttpURLConnection
@@ -16,58 +14,65 @@ data class TokenResponse(val token: String, val status: String)
 data class TokenCheckResponse(val edition: String, val status: String)
 
 class HardwareWalletURLRequestHandler {
-    private fun requestAuthToken(): String? {
-        try {
-            val url = URL("https://auth.hito.xyz/api/request_token")
-            val connection = url.openConnection() as HttpURLConnection
-            connection.requestMethod = "GET"
+    private suspend fun requestAuthToken(): String? {
+        return withContext(Dispatchers.IO) {
+            try {
+                val url = URL("https://auth.hito.xyz/api/request_token")
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "GET"
 
-            val response = connection.inputStream.bufferedReader().use { it.readText() }
+                val response = connection.inputStream.bufferedReader().use { it.readText() }
 
-            val tokenResponse = Gson().fromJson(response, TokenResponse::class.java)
+                val tokenResponse = Gson().fromJson(response, TokenResponse::class.java)
 
-            if (tokenResponse.status != "ok") {
-                Log.e("HitoAuth", "Token request failed: ${tokenResponse.status}")
-                throw IOException("Token request failed: ${tokenResponse.status}")
+                if (tokenResponse.status != "ok") {
+                    Log.e("HitoAuth", "Token request failed: ${tokenResponse.status}")
+                    throw IOException("Token request failed: ${tokenResponse.status}")
+                }
+                tokenResponse.token
+            } catch (e: Exception) {
+                Log.e("HitoAuth", "Error requesting token", e)
+                null
             }
-            return tokenResponse.token
-        } catch (e: Exception) {
-            Log.e("HitoAuth", "Error requesting token", e)
-            return null
         }
     }
 
-    fun createAuthPayload(): String {
-        val token = requestAuthToken() ?: throw IOException("Failed to retrieve auth token")
+    suspend fun createAuthPayload(): String? {
+        Log.d("HitoAuth", "Creating auth payload")
+        val token = requestAuthToken() ?: return null
+        Log.d("HitoAuth", "Received token: $token")
         val ts = Instant.now().epochSecond
         val roundedTs = (ts - (ts % 600)).toString()
         return "hito.auth:$roundedTs.${token}"
     }
 
-    fun authorizeToken(token: String): TokenCheckResponse {
-        try {
-            val url = URL("https://auth.hito.xyz/api/check_token?t=$token")
-            Log.d("HitoAuth.AuthorizeToken", "Requesting URL: $url")
-            val connection = url.openConnection() as HttpURLConnection
-            connection.requestMethod = "GET"
+    suspend fun authorizeToken(token: String): TokenCheckResponse {
+        return withContext(Dispatchers.IO) {
+            try {
+                val url = URL("https://auth.hito.xyz/api/check_token?t=$token")
+                Log.d("HitoAuth.AuthorizeToken", "Requesting URL: $url")
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "GET"
 
-            val response = connection.inputStream.bufferedReader().use { it.readText() }
-            return Gson().fromJson(response, TokenCheckResponse::class.java)
-        } catch (e: Exception) {
-            Log.e("HitoAuth", "Error authorizing token", e)
-            throw IOException("Error authorizing token: ${e.message}")
+                val response = connection.inputStream.bufferedReader().use { it.readText() }
+                Gson().fromJson(response, TokenCheckResponse::class.java)
+            } catch (e: Exception) {
+                Log.e("HitoAuth", "Error authorizing token", e)
+                throw IOException("Error authorizing token: ${e.message}")
+            }
         }
     }
 
-    fun getSolanaLatestBlockhash(endpoint: String? = null): String? {
-        try {
-            val url = URL(endpoint)
-            val connection = url.openConnection() as HttpURLConnection
-            connection.requestMethod = "POST"
-            connection.setRequestProperty("Content-Type", "application/json")
-            connection.doOutput = true
+    suspend fun getSolanaLatestBlockhash(endpoint: String? = null): String? {
+        return withContext(Dispatchers.IO) {
+            try {
+                val url = URL(endpoint)
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "POST"
+                connection.setRequestProperty("Content-Type", "application/json")
+                connection.doOutput = true
 
-            val requestBody = """
+                val requestBody = """
                 {
                 "jsonrpc":"2.0",
                 "id":1,
@@ -75,38 +80,44 @@ class HardwareWalletURLRequestHandler {
                 "params":[]
                 }
                 """.trimIndent()
-            connection.outputStream.use { os ->
-                os.write(requestBody.toByteArray(Charsets.UTF_8))
-            }
+                connection.outputStream.use { os ->
+                    os.write(requestBody.toByteArray(Charsets.UTF_8))
+                }
 
-            if (connection.responseCode == HttpURLConnection.HTTP_OK) {
-                val response = connection.inputStream.bufferedReader().use { it.readText() }
-                val json = JSONObject(response)
-                return json.getJSONObject("result").getJSONObject("value").getString("blockhash")
-            } else {
-                Log.e("HitoAuth", "Failed to get latest blockhash: ${connection.responseMessage}")
-                return null
+                if (connection.responseCode == HttpURLConnection.HTTP_OK) {
+                    val response = connection.inputStream.bufferedReader().use { it.readText() }
+                    val json = JSONObject(response)
+                    json.getJSONObject("result").getJSONObject("value")
+                        .getString("blockhash")
+                } else {
+                    Log.e(
+                        "HitoAuth",
+                        "Failed to get latest blockhash: ${connection.responseMessage}"
+                    )
+                    null
+                }
+            } catch (e: Exception) {
+                Log.e("HitoAuth", "Error getting latest blockhash", e)
+                null
             }
-        } catch (e: Exception) {
-            Log.e("HitoAuth", "Error getting latest blockhash", e)
-            return null
         }
     }
 
-    fun sendSolanaRawTransaction(
+    suspend fun sendSolanaRawTransaction(
         endpoint: String? = null,
         rawTransaction: String
     ): String? {
-        try {
-            val url = URL(endpoint)
-            val connection = url.openConnection() as HttpURLConnection
-            connection.requestMethod = "POST"
-            connection.setRequestProperty("Content-Type", "application/json")
-            connection.doOutput = true
+        return withContext(Dispatchers.IO) {
+            try {
+                val url = URL(endpoint)
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "POST"
+                connection.setRequestProperty("Content-Type", "application/json")
+                connection.doOutput = true
 
-            Log.d("HitoAuth.SendTransaction", "Using RPC source: $endpoint")
+                Log.d("HitoAuth.SendTransaction", "Using RPC source: $endpoint")
 
-            val requestBody = """
+                val requestBody = """
                 {
                 "jsonrpc":"2.0",
                 "id":1,
@@ -119,27 +130,34 @@ class HardwareWalletURLRequestHandler {
                 ]
                 }
                 """.trimIndent()
-            Log.d("HitoAuth.SendTransaction", "Request Body: $requestBody")
-            connection.outputStream.use { os ->
-                os.write(requestBody.toByteArray(Charsets.UTF_8))
-            }
-
-            if (connection.responseCode == HttpURLConnection.HTTP_OK) {
-                val response = connection.inputStream.bufferedReader().use { it.readText() }
-                val json = JSONObject(response)
-                Log.d("HitoAuth.SendTransaction", "Response: $json")
-                if (json.has("error")) {
-                    Log.e("HitoAuth.SendTransaction", "Error in response: ${json.getJSONObject("error").getString("message")}")
-                    return null
+                Log.d("HitoAuth.SendTransaction", "Request Body: $requestBody")
+                connection.outputStream.use { os ->
+                    os.write(requestBody.toByteArray(Charsets.UTF_8))
                 }
-                return json.getString("result")
-            } else {
-                Log.e("HitoAuth", "Failed to send raw transaction: ${connection.responseMessage}")
-                return null
+
+                if (connection.responseCode == HttpURLConnection.HTTP_OK) {
+                    val response = connection.inputStream.bufferedReader().use { it.readText() }
+                    val json = JSONObject(response)
+                    Log.d("HitoAuth.SendTransaction", "Response: $json")
+                    if (json.has("error")) {
+                        Log.e(
+                            "HitoAuth.SendTransaction",
+                            "Error in response: ${json.getJSONObject("error").getString("message")}"
+                        )
+                        null
+                    }
+                    json.getString("result")
+                } else {
+                    Log.e(
+                        "HitoAuth",
+                        "Failed to send raw transaction: ${connection.responseMessage}"
+                    )
+                    null
+                }
+            } catch (e: Exception) {
+                Log.e("HitoAuth", "Error sending raw transaction", e)
+                null
             }
-        } catch (e: Exception) {
-            Log.e("HitoAuth", "Error sending raw transaction", e)
-            return null
         }
     }
 }
